7329a33b09ba670cb2fc5471c3f059cad74704ab
Saimond-SEMS <exactsameghost@gmail.com>
2025-02-13 18:30:59 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace itla3password
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+		
+			const string user = "admin";
+			const string ps = "contra";
+			
+			Console.WriteLine("Bienvenido");
+
+			while (true)
+			{
+				Console.WriteLine("usuario:");
+				string yourUser = Console.ReadLine();
+				Console.WriteLine("Digite cotraseña");
+				string yourPsd = Console.ReadLine();
+				if ( user.Equals(yourUser) && ps.Equals(yourPsd))
+				{
+					Console.WriteLine("Acceso consedido");
+					break;
+				}
+				
+				Console.WriteLine("Contraseña o usuario incorrectos");
+				Console.WriteLine("--------------------------------");
+
+			}
+		}
+		
+	}
+}
